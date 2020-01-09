eb3a6be59369ab98d1492f7725cf2bff7dbb371f
Chance Carey <chance@carey.sh>
2020-01-09 13:39:15 +0200

    Add integration test watch mode

--- server/util.bash
+++ server/util.bash
@@ -4,15 +4,44 @@ set -e
 
 cd $(dirname $0)
 
-PREARGS="-f integration_tests.docker-compose.yml"
 POSTARGS="--build -V --abort-on-container-exit"
 
 function db_build {
-  docker-compose $PREARGS up $POSTARGS db
+  DC="integration_tests.docker-compose.yml"
+  docker-compose -f $DC up $POSTARGS db
 }
 
 function test {
-  docker-compose $PREARGS up $POSTARGS
+  DC="integration_tests.docker-compose.yml"
+  docker-compose -f $DC up $POSTARGS
+}
+
+function watch {
+  DC="integration_tests_watcher.docker-compose.yml"
+  docker-compose -f $DC build
+  tmux -f integration_tests/tmux.conf attach
+}
+
+function watch_db {
+  DC="integration_tests_watcher.docker-compose.yml"
+  docker-compose -f $DC up $POSTARGS db
+  docker-compose -f $DC down
+}
+
+function watch_core {
+  echo Waiting to start core
+  sleep 3
+  DC="integration_tests_watcher.docker-compose.yml"
+  docker-compose -f $DC up $POSTARGS core
+  docker-compose -f $DC down
+}
+
+function watch_integration_tests {
+  echo Waiting to start tests
+  sleep 6
+  DC="integration_tests_watcher.docker-compose.yml"
+  docker-compose -f $DC run --rm integration_tests
+  docker-compose -f $DC down
 }
 
 $@
