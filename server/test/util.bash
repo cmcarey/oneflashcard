09324c9e3fbf93cce38d3d09b203e4d4aad42b43
Chance Carey <chance@carey.sh>
2020-01-10 10:38:19 +0200

    Fix acceptance tests, needed to install deps

--- server/test/util.bash
+++ server/test/util.bash
@@ -11,26 +11,33 @@ function acceptance {
   DC_DEV="dev.docker-compose.yml"
 
   function test {
+    _install_deps
     docker-compose -f $DC up $POSTARGS
   }
 
   function dev {
+    _install_deps
     tmux -f tmux.conf attach
   }
 
-  function dev_db {
+  function _install_deps {
+    (cd src && yarn install)
+    (cd ../../core && yarn install)
+  }
+
+  function _dev_db {
     docker-compose -f $DC_DEV up $POSTARGS db
     docker-compose -f $DC_DEV down
   }
 
-  function dev_core {
+  function _dev_core {
     echo Starting core
     sleep 2
     docker-compose -f $DC_DEV up $POSTARGS core
     docker-compose -f $DC_DEV down
   }
 
-  function dev_acceptance {
+  function _dev_acceptance {
     echo Starting acceptance tests
     sleep 4
     docker-compose -f $DC_DEV run --rm acceptance
