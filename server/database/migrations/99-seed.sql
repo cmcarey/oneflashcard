afa241b04963adc20be3b5eeaf55e7bc25b3e896
Chance Carey <chance@carey.sh>
2020-01-31 17:59:23 +0000

    Remove manual seed data

--- server/database/migrations/99-seed.sql
+++
@@ -1,4 +0,0 @@
--- Hashed password of "somepass" for development.  need to fix this flow.
-INSERT INTO "users" (email, password) VALUES
-  ('chance@carey.sh',
-  '$2a$10$YL6tmZ4CQtXtf/YJc1GkhOBKapnNZOorFXXiJ5829qUlLgNHeXv..');
